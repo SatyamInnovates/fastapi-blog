883f33d38736a65f61836455737a381f687289c7
Satyam Kumar <your_email@example.com>
2026-05-30 16:25:07 +0530

    fixed bcrypt 72 byte limit

--- blog/hashing.py
+++ blog/hashing.py
@@ -4,7 +4,9 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto",bcrypt__truncate_er
 
 class Hash():
     def bcrypt(password: str):
+        password = password[:72]  
         return pwd_cxt.hash(password)
 
     def verify(plain_password, hashed_password):
+        plain_password = plain_password[:72]
         return pwd_cxt.verify(plain_password, hashed_password)
